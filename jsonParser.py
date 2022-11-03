9bbb4cac36c32c0fd75b6a0f3aa044d89a0cf7d9
Siddu Eswar <siddueshu@gmail.com>
2022-11-03 19:49:01 +0530

    updated data

---
+++ jsonParser.py
@@ -0,0 +1,29 @@
+from json import dump
+
+names = """
+Wade Mercer
+Sariah Hampton
+Marilyn Miranda
+Jessie Leach
+Gracie Malone
+Lexie Hardy
+Madden Perry
+Adrienne Bush
+Aydin Vargas
+Russell Johns
+Nataly Morse
+Yahir Mendoza
+Lara Bonilla
+Deborah Mason
+Angelique Jennings
+Rodrigo Newton
+Giselle Zamora
+Akira Munoz
+Mollie Mccormick
+Kimberly Pope
+"""
+jsonFile = open("names.json","w")
+data = []
+for name in names.split("\n")[1:-1]:
+    data.append(name.split())
+dump(data,jsonFile)
